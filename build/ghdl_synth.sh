e565e493127a2c2397f8856fb974a8fc2a48ac6c
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2026-05-19 21:24:17 +0000

    build: don't flatten design
    Previously the design needed to be flattened because GHDL crashed when
    synthesizing VHDL-2019 interfaces in port lists. This has been fixed
    upstream.
    
    Remaining issue is that Quartus does not play well with the [] in the
    generated extended identifiers. A simple regex matching replaces them
    with ().
    
    We still use Verilog as netlist because the blackbox handling in VHDL is
    incomplete with GHDL. Even though it synthesizes to a component
    instantiation, it turns the generics of e.g. type "STRING" into a
    "std_logic_vector" because it tries to only use primitive IEEE types.
    This is (rightfully) not accepted by Quartus.

--- build/ghdl_synth.sh
+++ build/ghdl_synth.sh
@@ -2,11 +2,34 @@
 # SPDX-License-Identifier: MIT
 set -e
 
-# Transpile VHDL-2008 source to Verilog netlist that Quartus understands.
+function postprocess_design () {
+    case "$1" in
+        *.v)
+            # Verilog
+
+            # Replace "\foo.bar[last] " with "\foo.bar(last) ".
+            sed -i -E 's/\\([^[]*)\[([^]]*)\] /\\\1(\2) /g' "$1"
+            ;;
+        *)
+            # VHDL
+
+            # Replace "\foo.bar[last]\" with "\foo.bar(last)\".
+            sed -i -E 's/\\([^\\]*)\[([^\\]*)\]\\/\\\1(\2)\\/g' "$1"
+
+            # Remove VHDL work libraries, they are not required as all types
+            # have been resolved to the primitive IEEE types.
+            sed -i -E 's/^use work\..*\.all;$//g' "$1"
+            ;;
+    esac
+}
+
+# Transpile VHDL-2019 source to Verilog netlist that Quartus understands.
 function synth () {
-    cmd="ghdl --synth --std=19 --no-formal --keep-hierarchy=no --out=verilog -o=$2 $1"
+    cmd="ghdl --synth --std=19 --no-formal --out=verilog -o=$2 $1"
     echo $cmd
     $cmd
+
+    postprocess_design $2
 }
 
 synth top top.synth.v
