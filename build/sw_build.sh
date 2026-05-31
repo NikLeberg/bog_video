46a3e726aba39b34ed7864268f67dc027095dd90
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2026-05-31 17:38:59 +0000

    sw: initial neorv32 firmware

---
+++ build/sw_build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+set -e
+
+make -C ../sw elf image
---
+++ sw/src/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author Niklaus Leuenberger <@NikLeberg>
+ * @brief Main application for NEORV32 processor.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <neorv32.h>
+
+int main(void) {
+}
