246eb920f9da4a09e88302eeff97f8270621255c
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2026-05-31 17:41:08 +0000

    src: split view into in/out records

--- src/top/top.vhd
+++ src/top/top.vhd
@@ -3,8 +3,10 @@
 -- Entity:                  top
 -- Author:                  Niklaus Leuenberger <@NikLeberg>
 -- SPDX-License-Identifier: MIT
--- Description:             Toplevel entity. Simple test of accessing data with
---                          Altera JTAG atom.
+-- Description:             Toplevel entity. End goal is to stream image data
+--                          from OV2640 camera through internal processing
+--                          pipeline and push it then to a computer through
+--                          JTAG.
 -- =============================================================================
 
 library ieee;
@@ -30,7 +32,7 @@ architecture rtl of top is
       INSTANCE_ID             : integer;
       LOG2_RXFIFO_DEPTH       : integer;
       LOG2_TXFIFO_DEPTH       : integer;
-      SLD_AUTO_INSTANCE_INDEX : string
+      SLD_AUTO_INSTANCE_INDEX : string;
     );
     port (
       clk   : in std_ulogic;
@@ -43,14 +45,15 @@ architecture rtl of top is
       t_dat : out std_ulogic_vector(7 downto 0);
       t_dav : in std_ulogic;  -- ready
       t_ena : out std_ulogic; -- valid
-      t_pause : out std_ulogic
+      t_pause : out std_ulogic;
     );
   end component;
 
   signal rst_ff : std_ulogic_vector(1 downto 0) := (others => '0');
   signal rst    : std_ulogic                    := '0';
 
-  signal atlantic_st, skiddeloo_st : st_t(data(7 downto 0));
+  signal atlantic_fwd, skid_fwd, stuff_fwd : st_fwd_t(data(7 downto 0));
+  signal atlantic_rev, skid_rev, stuff_rev : st_rev_t;
 
 begin
 
@@ -74,28 +77,43 @@ begin
       clk     => clk,
       rst_n   => not rst,
       -- data from FPGA --
-      r_dat   => skiddeloo_st.data,
-      r_val   => skiddeloo_st.valid,
-      r_ena   => skiddeloo_st.ready,
+      r_dat   => stuff_fwd.data,
+      r_val   => stuff_fwd.valid,
+      r_ena   => stuff_rev.ready,
       -- data to FPGA --
-      t_dat   => atlantic_st.data,
-      t_dav   => atlantic_st.ready,
-      t_ena   => atlantic_st.valid,
+      t_dat   => atlantic_fwd.data,
+      t_dav   => atlantic_rev.ready,
+      t_ena   => atlantic_fwd.valid,
       t_pause => open
     );
 
-  atlantic_st.last <= '0'; -- unused
+  atlantic_fwd.last <= '0'; -- unused
   
   st_skid_inst : entity work.st_skid
     generic map (
-      WIDTH => 8,
+      WIDTH  => 8,
       OUTREG => false
     )
     port map (
       clk    => clk,
       rst    => rst,
-      sink   => atlantic_st,
-      source => skiddeloo_st
+      us_fwd => atlantic_fwd,
+      us_rev => atlantic_rev,
+      ds_fwd => skid_fwd,
+      ds_rev => skid_rev
+    );
+
+  st_stuff_inst : entity work.st_stuff
+    generic map (
+      WIDTH => 8
+    )
+    port map (
+      clk    => clk,
+      rst    => rst,
+      us_fwd => skid_fwd,
+      us_rev => skid_rev,
+      ds_fwd => stuff_fwd,
+      ds_rev => stuff_rev
     );
 
 end architecture;
