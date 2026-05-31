462e14b36526fd73eb945b77b6a11a0b362df5f7
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2026-05-31 18:49:57 +0000

    src: move altera components to package

---
+++ src/alt/alt_pkg.vhd
@@ -0,0 +1,61 @@
+-- =============================================================================
+-- File:                    alt_pkg.vhd
+-- Package:                 alt_pkg
+-- Author:                  Niklaus Leuenberger <@NikLeberg>
+-- SPDX-License-Identifier: MIT
+-- Description:             Collection of Altera FPGA specific black boxes.
+-- =============================================================================
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package alt_pkg is
+
+  -- Altera Cycline IVE JTAG primitive.
+  -- See: https://tomverbeure.github.io/2021/10/30/Intel-JTAG-Primitive.html
+  component cycloneive_jtag
+    generic (
+      lpm_type : string := "cycloneive_jtag"
+    );
+    port (
+      tms         : in std_ulogic := '0';
+      tck         : in std_ulogic := '0';
+      tdi         : in std_ulogic := '0';
+      tdouser     : in std_ulogic := '0';
+      tdo         : out std_ulogic;
+      tmsutap     : out std_ulogic;
+      tckutap     : out std_ulogic;
+      tdiutap     : out std_ulogic;
+      shiftuser   : out std_ulogic;
+      clkdruser   : out std_ulogic;
+      updateuser  : out std_ulogic;
+      runidleuser : out std_ulogic;
+      usr1user    : out std_ulogic;
+    );
+  end component;
+
+  -- (Undocumented) Altera megafunction that lets you send/receive arbitraty
+  -- data through JTAG via the SLD system.
+  -- See: https://tomverbeure.github.io/2021/05/08/Write-Your-Own-C-and-Python-Clients-for-Intel-JTAG-UART-with-libjtag_atlantic.html
+  component alt_jtag_atlantic is
+    generic (
+      INSTANCE_ID             : integer;
+      LOG2_RXFIFO_DEPTH       : integer;
+      LOG2_TXFIFO_DEPTH       : integer;
+      SLD_AUTO_INSTANCE_INDEX : string;
+    );
+    port (
+      clk   : in std_ulogic;
+      rst_n : in std_ulogic;
+      -- data from FPGA --
+      r_dat : in std_ulogic_vector(7 downto 0);
+      r_val : in std_ulogic;  -- valid
+      r_ena : out std_ulogic; -- ready
+      -- data to FPGA --
+      t_dat   : out std_ulogic_vector(7 downto 0);
+      t_dav   : in std_ulogic;  -- ready
+      t_ena   : out std_ulogic; -- valid
+      t_pause : out std_ulogic;
+    );
+  end component;
+end package;
--- src/top/top.vhd
+++ src/top/top.vhd
@@ -15,6 +15,7 @@ use ieee.numeric_std.all;
 
 library neorv32;
 
+use work.alt_pkg.all;
 use work.st_pkg.all;
 
 entity top is
@@ -39,49 +40,6 @@ entity top is
 end entity;
 
 architecture rtl of top is
-
-  component cycloneive_jtag
-    generic (
-      lpm_type : string := "cycloneive_jtag"
-    );
-    port (
-      tms         : in std_ulogic := '0';
-      tck         : in std_ulogic := '0';
-      tdi         : in std_ulogic := '0';
-      tdouser     : in std_ulogic := '0';
-      tdo         : out std_ulogic;
-      tmsutap     : out std_ulogic;
-      tckutap     : out std_ulogic;
-      tdiutap     : out std_ulogic;
-      shiftuser   : out std_ulogic;
-      clkdruser   : out std_ulogic;
-      updateuser  : out std_ulogic;
-      runidleuser : out std_ulogic;
-      usr1user    : out std_ulogic;
-    );
-  end component;
-
-  component alt_jtag_atlantic is
-    generic (
-      INSTANCE_ID             : integer;
-      LOG2_RXFIFO_DEPTH       : integer;
-      LOG2_TXFIFO_DEPTH       : integer;
-      SLD_AUTO_INSTANCE_INDEX : string;
-    );
-    port (
-      clk   : in std_ulogic;
-      rst_n : in std_ulogic;
-      -- data from FPGA --
-      r_dat : in std_ulogic_vector(7 downto 0);
-      r_val : in std_ulogic;  -- valid
-      r_ena : out std_ulogic; -- ready
-      -- data to FPGA --
-      t_dat : out std_ulogic_vector(7 downto 0);
-      t_dav : in std_ulogic;  -- ready
-      t_ena : out std_ulogic; -- valid
-      t_pause : out std_ulogic;
-    );
-  end component;
 
   signal rst_ff : std_ulogic_vector(1 downto 0) := (others => '0');
   signal rst    : std_ulogic                    := '0';
